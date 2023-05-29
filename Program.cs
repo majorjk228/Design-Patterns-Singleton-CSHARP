67e2561997c04e1e396f5999b5b7c9171137b42a
sana-love-cs sana-love-cs <sana-ivanov1@yandex.ru>
2023-05-29 19:09:14 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+namespace Design_Patterns_Singleton_CSHARP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.Initialize(); // Создаем объект
+
+            Singleton s2 = Singleton.Initialize(); // Присваеваем тот же объект (s1)
+
+            Console.WriteLine(s1.Equals(s2));
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,21 @@
+﻿namespace Design_Patterns_Singleton_CSHARP;
+
+public class Singleton
+{
+    private static Singleton single = null;
+
+    protected Singleton() // Объект класса мы можем создать только через метод 
+    {
+        
+    }
+
+    public static Singleton Initialize() // Создаем метод
+    {
+        if (single == null)
+        {
+            single = new Singleton();
+        }
+
+        return single;
+    }
+}
